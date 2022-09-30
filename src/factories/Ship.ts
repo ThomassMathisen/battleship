1504d65d989339639ddbf63d904835008a432470
ThomassMathisen <thomasmathisen@hotmail.com>
2022-09-30 15:40:55 +0200

    Setup Ship & Battleship functionality

---
+++ src/factories/Ship.ts
@@ -0,0 +1,19 @@
+export default class Ship {
+  length: number
+  hits: number[]
+
+  constructor(length: number) {
+    this.length = length
+    this.hits = []
+  }
+
+  hit(position: number) {
+    if (this.hits.includes(position) || position < 0 || position >= this.length)
+      return
+    this.hits.push(position)
+  }
+
+  isSunk() {
+    return this.hits.length === this.length
+  }
+}
