3a77f8355d431338cc1f3a9455b37e930bde31e6
ThomassMathisen <thomasmathisen@hotmail.com>
2022-09-30 17:59:01 +0200

    Add Player and Player tests

---
+++ src/factories/tests/Player.test.ts
@@ -0,0 +1,36 @@
+import Gameboard from "../Gameboard";
+import Player from "../Player";
+import Ship from "../Ship";
+
+describe('Player', () => {
+  let player: Player
+  let gameboard: Gameboard
+  let ship: Ship
+
+    beforeEach(() => {
+      player = new Player('p1')
+      gameboard = new Gameboard()
+      ship = new Ship(3)
+    })
+
+    test('creates and initialize an object', () => {
+      expect(player).toEqual({ name: 'p1', alreadyHitCoords: [] })
+    })
+
+    test('attacks', () => {
+      gameboard.placeShip(ship, 1, 1, true)
+      player.attack(1, 1, gameboard)
+      player.attack(2, 1, gameboard)
+      player.attack(3, 1, gameboard)
+      expect(gameboard.isGameOver()).toBe(true)
+    })
+
+    test('random attack', () => {
+      gameboard.placeShip(ship, 1, 1, true)
+      for (let i = 1; i < 100; i++) {
+        player.randomAttack(gameboard)
+      }
+      expect(gameboard.isGameOver()).toBe(true)
+    })
+})
+
